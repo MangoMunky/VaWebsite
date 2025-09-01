4750ad04557e4607b58e13a1037547dd87662cfd
MangoMunky <77705856+MangoMunky@users.noreply.github.com>
2025-09-01 05:38:45 +0000

    Javascript night mode button

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+function darkMode() {
+    var bodyElement = document.body;
+    var button = document.getElementById("darkMode");
+    var emojiSpan = button.querySelector(".emoji-icon");
+
+    bodyElement.classList.toggle("darkMode");
+    emojiSpan.classList.add("hidden");
+
+    setTimeout(function() {
+        if (bodyElement.classList.contains("darkMode")) {
+            emojiSpan.textContent = "☀️";
+        } else {
+            emojiSpan.textContent = "🌙";
+        }
+        emojiSpan.classList.remove("hidden");
+    }, 200);
+}
+
+
+/*
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
+var curTi = today.toLocaleString();
 
-if (hourNow>0){
-    greeting='yo'
+if (curTi<=20 && curTi>=6){
+    greeting='good morning'
 }
 else{
-    greeting="maaa"
+    greeting="good night"
+    document.body.style.backgroundColor = "#161001";
 }
 
-var formattedDate = today.toLocaleString();
-document.write('<h3>' + greeting + formattedDate + '</h3>');
+
+
+document.write('<h3>' + greeting + curTi + '</h3>');
+*/
